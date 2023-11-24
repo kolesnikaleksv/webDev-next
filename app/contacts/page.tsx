2a1a5995ce2d27c77606c0f308446a130c2d4689
kolesnikaleksv <kolesnikaleksv@gmail.com>
2023-11-24 13:41:11 +0200

    Add fetching data to contacts page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,18 +1,34 @@
 import Heading, {HeadingProps} from "@/components/Heading";
 import type { Metadata } from 'next'
- 
+
+interface ContactsProps {
+  id: number;
+  name: string;
+  email: string;
+}
+
 export const metadata: Metadata = {
   title: 'Contacts page',
   description: 'Page for contacts',
 }
 
-const Contacts = () => {
+const Contacts = async () => {
+  const staticData = await fetch(`https://jsonplaceholder.typicode.com/users`, { cache: 'force-cache' });
+  const data: ContactsProps[] = await staticData.json();
+
   const headingProps: HeadingProps = {
     text: "List of contacts:"
   }
   return (
     <>
       <Heading {...headingProps} />
+      <ul>
+        {
+          data && data.map(({id, name, email}) => (
+            <li key={id}>{name}<strong>({email})</strong></li>
+          ))
+        }
+      </ul>
     </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={montserratAlternates.className}>
         <Layout>
-          <div className='flex-1'>
+          <div className='flex-1 p-12'>
             {children}
           </div>
         </Layout>
